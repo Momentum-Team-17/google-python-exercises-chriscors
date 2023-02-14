fcd76b05fffbbc3a88a3912ec083c198c5452a47
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-14 17:51:33 -0500

    Completes challenges 2 & 3

--- basic/list1.py
+++ basic/list1.py
@@ -36,9 +36,22 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    words.sort()
+    return sorted(words, key=is_x, reverse=True)
 
+    # or like they implied
+    xs = []
+    others = []
+    for item in words:
+        if item[0] == "x":
+            xs.append(item)
+        else:
+            others.append(item)
+    return sorted(xs) + sorted(others)
+
+
+def is_x(word):
+    return word[0] == "x"
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
@@ -46,13 +59,19 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
-def sort_last(tuples):
-    # +++your code here+++
-    return
 
+
+def sort_last(tuples):
+    return sorted(tuples, key=tuple)
+
+
+def tuple(tup):
+    return tup[-1]
 
 # Simple provided test() function used in main() to print(
 # what each function returns vs. what it's supposed to return.
+
+
 def test(got, expected):
     if got == expected:
         prefix = ' OK '
